8815480c4a068a79e97e7745e9757c5fb1e34221
unknown <lim337slaa@hanmail.net>
2021-10-26 21:50:30 +0900

    Mission 2 까지 완료

--- src/Pages/kibum/Login/Login.js
+++ src/Pages/kibum/Login/Login.js
@@ -6,6 +6,35 @@ class Login extends React.Component {
     this.props.history.push('/Main');
   };
 
+  constructor() {
+    super();
+    this.state = {
+      inputId: '',
+      inputPw: '',
+      btnActive: false,
+    };
+  }
+  handleIdInput = e => {
+    this.setState({
+      inputId: e.target.value,
+    });
+    this.isOkayToGo();
+  };
+  handlePwInput = e => {
+    this.setState({
+      inputPw: e.target.value,
+    });
+    this.isOkayToGo();
+  };
+
+  isOkayToGo = () => {
+    if (this.state.inputId.includes('@') && this.state.inputPw.length > 5) {
+      this.state.btnActive = true;
+    } else {
+      this.state.btnActive = false;
+    }
+  };
+
   render() {
     return (
       <main className="loginContainer">
@@ -13,15 +42,26 @@ class Login extends React.Component {
           <h1>Westagram</h1>
           <form className="loginForm">
             <input
+              onChange={this.handleIdInput}
+              name="inputId"
               id="id"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
             />
-            <input id="pw" type="password" placeholder="비밀번호" />
+            <input
+              onChange={this.handlePwInput}
+              name="inputPw"
+              id="pw"
+              type="password"
+              placeholder="비밀번호"
+            />
             <button
+              disabled={this.state.btnActive}
               onClick={this.goToMain}
               id="loginButton"
-              style={{ color: 'white' }}
+              style={{
+                backgroundColor: this.state.btnActive ? 'red' : 'white',
+              }}
             >
               로그인
             </button>
